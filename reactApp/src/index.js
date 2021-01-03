a477ea677e3a5f234dc8b08a8c50721c3e053098
Karol Uju Amamjirionwu <a.karoluju97@gmail.com>
2021-01-03 23:44:00 +0000

    Topic 11 - I've completed all my labs!

--- reactApp/src/index.js
+++ reactApp/src/index.js
@@ -1,1 +1,11 @@
 import './sample/';
+import MovieProvider from "./moviesContext";
+
+
+    <MovieProvider>
+      <Switch>
+                ...
+
+                ...
+      </Switch>
+    </MovieProvider>
